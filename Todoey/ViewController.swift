c46febb39e7d3d88746327466a07abcd4b9ddb5a
Gokhan Ugurel <gokhanugurel@gokhan-macbook-air.local>
2019-02-09 12:06:48 +0300

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Gokhan Ugurel on 9.02.2019.
+//  Copyright © 2019 Gokhan Ugurel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
