5f6d92e5a1bff05a53290fa274d47e201de48045
Gokhan Ugurel <gokhanugurel@gokhan-macbook-air.local>
2019-02-16 19:52:00 +0300

    Convert to MVC design pattern

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  ItemModel.swift
+//  Todoey
+//
+//  Created by Gokhan Ugurel on 16.02.2019.
+//  Copyright © 2019 Gokhan Ugurel. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
